50261724bbe40f8e78068c799cd6bae446df0c7b
Marcin Woźniak <marcinwozniak407@gmail.com>
2022-09-22 19:49:24 +0200

    Add AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: Scaffold(appBar: AppBar(title: const Text('When was that?'),),
     );
   }
+  
 }
