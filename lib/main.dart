da2f4f8095e841647aaf77a0067add5e62768100
Marcin Woźniak <marcinwozniak407@gmail.com>
2022-10-09 19:50:39 +0200

    Install FirebaseAuth

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -40,12 +41,41 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('When was that?'),
-      ),
-      body: Image.asset('assets/icon/icon.png'),
-    );
+    return const RootPage();
+  }
+}
+
+class RootPage extends StatelessWidget {
+  const RootPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          final user = snapshot.data;
+          if (user == null) {
+            return Scaffold(
+              appBar: AppBar(
+                centerTitle: true,
+                title: const Text("When was that?"),
+              ),
+              body: const Center(
+                child: Text("Jesteś niezalogowany"),
+              ),
+            );
+          }
+          return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: const Text('When was that?'),
+            ),
+            body: Center(
+              child: Text('Jesteś zalogowany jako ${user.email}'),
+            ),
+          );
+        });
   }
 }
