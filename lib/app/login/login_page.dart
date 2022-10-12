e05650f3837233da5c1bfdbab2cb647851212995
Marcin Woźniak <marcinwozniak407@gmail.com>
2022-10-12 21:07:46 +0200

    Add elements to LoginPage

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({
+  LoginPage({
     Key? key,
   }) : super(key: key);
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +17,45 @@ class LoginPage extends StatelessWidget {
         centerTitle: true,
         title: const Text("When was that?"),
       ),
-      body: const Center(
-        child: Text("Jesteś niezalogowany"),
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Image(
+                image: AssetImage('assets/icon/icon.png'),
+              ),
+              Text(
+                'SIGN IN',
+                style: GoogleFonts.notoSerif(fontSize: 40, color: Colors.blue),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(hintText: "e-mail"),
+              ),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(hintText: "password"),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: emailController.text,
+                    password: passwordController.text,
+                  );
+                },
+                child: const Text('Sign in'),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
