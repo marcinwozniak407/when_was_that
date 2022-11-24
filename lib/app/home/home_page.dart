b77eaeea278d5e950dfe2138726dfaa210e35285
Marcin Woźniak <marcinwozniak407@gmail.com>
2022-11-24 20:00:26 +0100

    Add currentIndex to BottomNavigationBar

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,13 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     Key? key,
     required this.user,
   }) : super(key: key);
 
   final User user;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +24,15 @@ class HomePage extends StatelessWidget {
         title: const Text('When was that?'),
       ),
       body: Center(
-        child: Text('Jesteś zalogowany jako ${user.email}'),
+        child: Text('Jesteś zalogowany jako ${widget.user.email}'),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (NewIndex) {
+          setState(() {
+            currentIndex = NewIndex;
+          });
+        },
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.add),
