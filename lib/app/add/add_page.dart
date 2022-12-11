66e469076e4f2ac3bb5d755cb4886edd156cd4a7
Marcin Woźniak <marcinwozniak407@gmail.com>
2022-12-11 12:54:58 +0100

    Remove unnecessary code in AddPage

--- lib/app/add/add_page.dart
+++ lib/app/add/add_page.dart
@@ -1,16 +1,2 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-class AddPage extends StatefulWidget {
-  const AddPage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<AddPage> createState() => _AddPageState();
-
-  class _AddPageState extends State<AddPage> {
-    String? imageURL;
-    String? title;
-    DateTime? date;
-  }
