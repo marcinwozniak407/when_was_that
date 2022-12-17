4e77f43bd1645c3be1bab5446d5fa4c65b7559e4
Marcin Woźniak <marcinwozniak407@gmail.com>
2022-12-17 17:15:10 +0100

    Organise files

--- lib/app/list/list_page_content.dart
+++ lib/app/list/list_page_content.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+
+class ListPageContent extends StatelessWidget {
+  const ListPageContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('List', style: TextStyle(fontSize: 50)));
+  }
+}
