6bec34cf31cb62d1de6155d04ffbe0b8b521b6b6
Marcin Woźniak <marcinwozniak407@gmail.com>
2022-12-17 17:17:16 +0100

    Organise Add file

--- lib/app/add/add_page_content.dart
+++ lib/app/add/add_page_content.dart
@@ -1,2 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+class AddPageContent extends StatelessWidget {
+  const AddPageContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('events').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text("Something went wrong..."),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: Text('Loading...'),
+            );
+          }
+
+          return const Center(
+              child: Text('Add', style: TextStyle(fontSize: 50)));
+        });
+  }
+}
