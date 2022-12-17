cdf12f20ca7daed641baff73c57549498d1d7136
Marcin Woźniak <marcinwozniak407@gmail.com>
2022-12-17 17:44:35 +0100

    Add import data functionality in ListPageContent

--- lib/app/list/list_page_content.dart
+++ lib/app/list/list_page_content.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ListPageContent extends StatelessWidget {
@@ -7,6 +8,31 @@ class ListPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('List', style: TextStyle(fontSize: 50)));
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('events').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text("Something went wrong..."),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: Text('Loading...'),
+            );
+          }
+
+          final documents = snapshot.data!.docs;
+          return ListView(
+            children: [
+              for (final document in documents) ...[
+                Text(document['category']),
+                Text(document['name']),
+                Text(document['comment']),
+                Text(document['rating'].toString()),
+              ],
+            ],
+          );
+        });
   }
 }
