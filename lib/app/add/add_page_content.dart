34f87c21c386d8cc73e5a04a3a2a7be3a621c1c5
Marcin Woźniak <marcinwozniak407@gmail.com>
2022-12-18 14:11:23 +0100

    Improve the visuality of AddPageContent

--- lib/app/add/add_page_content.dart
+++ lib/app/add/add_page_content.dart
@@ -20,85 +20,93 @@ class _AddPageContentState extends State<AddPageContent> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            onChanged: (newValue) {
-              setState(() {
-                categoryOfEvent = newValue;
-              });
-            },
-            decoration: InputDecoration(
-              hintText: "category of event",
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(color: Colors.blue),
-                borderRadius: BorderRadius.circular(12),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          TextField(
-            onChanged: (newValue) {
-              setState(() {
-                eventName = newValue;
-              });
-            },
-            decoration: InputDecoration(
-              hintText: "name of event",
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(color: Colors.blue),
-                borderRadius: BorderRadius.circular(12),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          TextField(
-            onChanged: (newValue) {
-              setState(() {
-                comment = newValue;
-              });
-            },
-            decoration: InputDecoration(
-              hintText: "add a comment to this event if you want",
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(color: Colors.blue),
-                borderRadius: BorderRadius.circular(12),
-              ),
-            ),
-          ),
-          Slider(
-            value: rating,
-            onChanged: (newValue) {
-              setState(() {
-                rating = newValue;
-              });
-            },
-            min: 1.0,
-            max: 6.0,
-            divisions: 10,
-            label: rating.toString(),
-          ),
-          const SizedBox(height: 20),
-          Container(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {
-                FirebaseFirestore.instance.collection('events').add({
-                  'category': categoryOfEvent,
-                  'name': eventName,
-                  'comment': comment,
-                  'rating': rating,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              onChanged: (newValue) {
+                setState(() {
+                  categoryOfEvent = newValue;
                 });
               },
-              child: const Text('Add event'),
+              decoration: InputDecoration(
+                hintText: "category of event",
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              onChanged: (newValue) {
+                setState(() {
+                  eventName = newValue;
+                });
+              },
+              decoration: InputDecoration(
+                hintText: "name of event",
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              onChanged: (newValue) {
+                setState(() {
+                  comment = newValue;
+                });
+              },
+              decoration: InputDecoration(
+                hintText: "add a comment to this event if you want",
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Slider(
+              value: rating,
+              onChanged: (newValue) {
+                setState(() {
+                  rating = newValue;
+                });
+              },
+              min: 1.0,
+              max: 6.0,
+              divisions: 10,
+              label: rating.toString(),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              height: 45,
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  FirebaseFirestore.instance.collection('events').add({
+                    'category': categoryOfEvent,
+                    'name': eventName,
+                    'comment': comment,
+                    'rating': rating,
+                  });
+                },
+                child: const Text(
+                  'Add event',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
