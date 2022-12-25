fd88e6a481709113a98e89f996d1164427e6e37f
Marcin Woźniak <marcinwozniak407@gmail.com>
2022-12-25 11:42:26 +0100

    Set documents in Container

--- lib/app/list/list_page_content.dart
+++ lib/app/list/list_page_content.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ListPageContent extends StatelessWidget {
   const ListPageContent({
@@ -26,16 +27,39 @@ class ListPageContent extends StatelessWidget {
           }
 
           final documents = snapshot.data!.docs;
-          return ListView(
-            children: [
-              for (final document in documents) ...[
-                Text(document['category'].toUpperCase()),
-                Text(document['name']),
-                Text(document['date'].toString()),
-                Text(document['comment']),
-                Text(document['rating'].toString()),
-              ],
-            ],
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 30,
+            ),
+            child: Container(
+              height: 140,
+              decoration: const BoxDecoration(color: Colors.green),
+              margin: const EdgeInsets.all(10),
+              child: ListView(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                children: [
+                  for (final document in documents) ...[
+                    Text(document['category'].toUpperCase(),
+                        style: GoogleFonts.roboto(
+                            fontSize: 20, color: Colors.white)),
+                    Text(document['name'],
+                        style: GoogleFonts.robotoCondensed(
+                            fontSize: 18, color: Colors.white)),
+                    Text(document['date'].toString(),
+                        style: GoogleFonts.robotoCondensed(
+                            fontSize: 15, color: Colors.white)),
+                    Text(document['comment'],
+                        style: GoogleFonts.robotoCondensed(
+                            fontSize: 15, color: Colors.white)),
+                    Text(document['rating'].toString(),
+                        style: GoogleFonts.robotoCondensed(
+                            fontSize: 15, color: Colors.white)),
+                  ],
+                ],
+              ),
+            ),
           );
         });
   }
