0d7710e4f758850d9d784ad1a54b6ab9348c4150
Marcin Woźniak <marcinwozniak407@gmail.com>
2022-12-26 13:22:14 +0100

    Add loading and error variables

--- lib/app/events/cubit/events_state.dart
+++ lib/app/events/cubit/events_state.dart
@@ -3,6 +3,12 @@ part of 'events_cubit.dart';
 @immutable
 class EventsState {
   final List<QueryDocumentSnapshot<Object?>> documents;
+  final bool isLoading;
+  final String errorMessage;
 
-  EventsState({required this.documents});
+  const EventsState({
+    required this.documents,
+    required this.isLoading,
+    required this.errorMessage,
+  });
 }
