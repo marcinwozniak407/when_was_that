c1922e79da47e632683d404dbffeacd4cd57e07f
Marcin Woźniak <marcinwozniak407@gmail.com>
2022-12-26 13:05:06 +0100

    Add bloc provider

--- lib/app/events/events_page_content.dart
+++ lib/app/events/events_page_content.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:when_was_that/app/events/cubit/events_cubit.dart';
 
 class EventsPageContent extends StatelessWidget {
   const EventsPageContent({
@@ -9,58 +11,61 @@ class EventsPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('events')
-            .orderBy('rating', descending: true)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(
-              child: Text("Something went wrong..."),
-            );
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: Text('Loading...'),
-            );
-          }
+    return BlocProvider(
+      create: (context) => EventsCubit(),
+      child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('events')
+              .orderBy('rating', descending: true)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Center(
+                child: Text("Something went wrong..."),
+              );
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: Text('Loading...'),
+              );
+            }
 
-          final documents = snapshot.data!.docs;
-          return Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 10,
-              horizontal: 30,
-            ),
-            child: Container(
-              height: 140,
-              decoration: const BoxDecoration(color: Colors.green),
-              margin: const EdgeInsets.all(10),
-              child: ListView(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                children: [
-                  for (final document in documents) ...[
-                    Text(document['category'].toUpperCase(),
-                        style: GoogleFonts.roboto(
-                            fontSize: 20, color: Colors.white)),
-                    Text(document['name'],
-                        style: GoogleFonts.robotoCondensed(
-                            fontSize: 18, color: Colors.white)),
-                    Text(document['date'].toString(),
-                        style: GoogleFonts.robotoCondensed(
-                            fontSize: 15, color: Colors.white)),
-                    Text(document['comment'],
-                        style: GoogleFonts.robotoCondensed(
-                            fontSize: 15, color: Colors.white)),
-                    Text(document['rating'].toString(),
-                        style: GoogleFonts.robotoCondensed(
-                            fontSize: 15, color: Colors.white)),
-                  ],
-                ],
+            final documents = snapshot.data!.docs;
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 30,
               ),
-            ),
-          );
-        });
+              child: Container(
+                height: 140,
+                decoration: const BoxDecoration(color: Colors.green),
+                margin: const EdgeInsets.all(10),
+                child: ListView(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                  children: [
+                    for (final document in documents) ...[
+                      Text(document['category'].toUpperCase(),
+                          style: GoogleFonts.roboto(
+                              fontSize: 20, color: Colors.white)),
+                      Text(document['name'],
+                          style: GoogleFonts.robotoCondensed(
+                              fontSize: 18, color: Colors.white)),
+                      Text(document['date'].toString(),
+                          style: GoogleFonts.robotoCondensed(
+                              fontSize: 15, color: Colors.white)),
+                      Text(document['comment'],
+                          style: GoogleFonts.robotoCondensed(
+                              fontSize: 15, color: Colors.white)),
+                      Text(document['rating'].toString(),
+                          style: GoogleFonts.robotoCondensed(
+                              fontSize: 15, color: Colors.white)),
+                    ],
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
   }
 }
