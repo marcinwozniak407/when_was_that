bd4d058794d7eb7003ab6babac72dd31ca6638ae
Marcin Woźniak <marcinwozniak407@gmail.com>
2022-12-26 12:56:19 +0100

    Set state in cubit

--- lib/app/events/cubit/events_cubit.dart
+++ lib/app/events/cubit/events_cubit.dart
@@ -1,8 +1,9 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
 
 part 'events_state.dart';
 
 class EventsCubit extends Cubit<EventsState> {
-  EventsCubit() : super(EventsState());
+  EventsCubit() : super(EventsState(documents: const []));
 }
