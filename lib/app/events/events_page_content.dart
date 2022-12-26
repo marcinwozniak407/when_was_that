80441b702c84355b50bd0c8db6a2b27a31d373cf
Marcin Woźniak <marcinwozniak407@gmail.com>
2022-12-26 12:27:18 +0100

    Change word list into events

--- lib/app/events/events_page_content.dart
+++ lib/app/events/events_page_content.dart
@@ -2,8 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class ListPageContent extends StatelessWidget {
-  const ListPageContent({
+class EventsPageContent extends StatelessWidget {
+  const EventsPageContent({
     Key? key,
   }) : super(key: key);
 
