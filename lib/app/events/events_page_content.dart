bc27af3d64ffaedffc951e52c54172bc6d102583
Marcin Woźniak <marcinwozniak407@gmail.com>
2023-01-14 19:33:08 +0100

    Add oportunity to delete item

--- lib/app/events/events_page_content.dart
+++ lib/app/events/events_page_content.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -34,61 +35,70 @@ class EventsPageContent extends StatelessWidget {
             ),
             children: [
               for (final document in documents)
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.lightGreen[300],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(document['category'].toUpperCase(),
-                            style: GoogleFonts.roboto(
-                                fontSize: 15, color: Colors.black)),
-                        Container(
-                          decoration:
-                              const BoxDecoration(color: Colors.white70),
-                          margin: const EdgeInsets.symmetric(vertical: 5),
-                          padding: const EdgeInsets.all(5),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  document['name'],
-                                  style: GoogleFonts.robotoCondensed(
-                                      fontSize: 25,
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              Align(
-                                alignment: Alignment.centerRight,
-                                child: Text(
-                                    document['rating'].toStringAsFixed(0),
-                                    style: GoogleFonts.robotoSlab(
-                                        fontSize: 25, color: Colors.black)),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Text(document['date'].toString(),
-                            style: GoogleFonts.robotoCondensed(
-                                fontSize: 15, color: Colors.black)),
-                        if (document['comment'].isNotEmpty)
+                Dismissible(
+                  key: ValueKey(document.id),
+                  onDismissed: (_) {
+                    FirebaseFirestore.instance
+                        .collection('events')
+                        .doc(document.id)
+                        .delete();
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.lightGreen[300],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(document['category'].toUpperCase(),
+                              style: GoogleFonts.roboto(
+                                  fontSize: 15, color: Colors.black)),
                           Container(
                             decoration:
                                 const BoxDecoration(color: Colors.white70),
                             margin: const EdgeInsets.symmetric(vertical: 5),
                             padding: const EdgeInsets.all(5),
-                            child: Text(document['comment'].toString(),
-                                style: GoogleFonts.greatVibes(
-                                  fontSize: 25,
-                                  color: Colors.black,
-                                )),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    document['name'],
+                                    style: GoogleFonts.robotoCondensed(
+                                        fontSize: 25,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Text(
+                                      document['rating'].toStringAsFixed(0),
+                                      style: GoogleFonts.robotoSlab(
+                                          fontSize: 25, color: Colors.black)),
+                                ),
+                              ],
+                            ),
                           ),
-                      ],
+                          Text(document['date'].toString(),
+                              style: GoogleFonts.robotoCondensed(
+                                  fontSize: 15, color: Colors.black)),
+                          if (document['comment'].isNotEmpty)
+                            Container(
+                              decoration:
+                                  const BoxDecoration(color: Colors.white70),
+                              margin: const EdgeInsets.symmetric(vertical: 5),
+                              padding: const EdgeInsets.all(5),
+                              child: Text(document['comment'].toString(),
+                                  style: GoogleFonts.greatVibes(
+                                    fontSize: 25,
+                                    color: Colors.black,
+                                  )),
+                            ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
