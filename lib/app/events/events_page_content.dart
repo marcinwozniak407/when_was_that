a2f320a04bcd0e17be65a2083030dc338e62a910
Marcin Woźniak <marcinwozniak407@gmail.com>
2023-01-16 19:57:28 +0100

    Implement show search message

--- lib/app/events/events_page_content.dart
+++ lib/app/events/events_page_content.dart
@@ -33,7 +33,12 @@ class EventsPageContent extends StatelessWidget {
               centerTitle: true,
               title: const Text("Events"),
               actions: [
-                IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    showSearch(context: context, delegate: MySearchDelegate());
+                  },
+                )
               ],
             ),
             body: ListView(
@@ -126,5 +131,57 @@ class EventsPageContent extends StatelessWidget {
         },
       ),
     );
+  }
+}
+
+class MySearchDelegate extends SearchDelegate {
+  List<String> searchResults = [];
+
+  @override
+  Widget? buildLeading(BuildContext context) => IconButton(
+        onPressed: () => close(context, null), //close searchbar
+        icon: const Icon(Icons.arrow_back),
+      );
+
+  @override
+  List<Widget>? buildActions(BuildContext context) => [
+        IconButton(
+          onPressed: () {
+            if (query.isEmpty) {
+              close(context, null);
+            } //close searchbar
+            else {
+              query = '';
+            }
+          },
+          icon: const Icon(Icons.clear),
+        ),
+      ];
+
+  @override
+  Widget buildResults(BuildContext context) => Center(child: Text(query));
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestions = searchResults.where((searchResult) {
+      final result = searchResult.toLowerCase();
+      final input = query.toLowerCase();
+
+      return result.contains(input);
+    }).toList();
+
+    return ListView.builder(
+        itemCount: suggestions.length,
+        itemBuilder: (context, index) {
+          final suggestion = suggestions[index];
+
+          return ListTile(
+              title: Text(suggestion),
+              onTap: () {
+                query = suggestion;
+
+                showResults(context);
+              });
+        });
   }
 }
