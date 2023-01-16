60e899b529321b81d8f4a64f81c8e32eed1d8e4b
Marcin Woźniak <marcinwozniak407@gmail.com>
2023-01-16 20:12:01 +0100

    Add necessary import

--- lib/app/events/events_page_content.dart
+++ lib/app/events/events_page_content.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:when_was_that/app/events/cubit/events_cubit.dart';
+import 'package:when_was_that/app/search/search.dart';
 
 class EventsPageContent extends StatelessWidget {
   const EventsPageContent({
@@ -131,57 +132,5 @@ class EventsPageContent extends StatelessWidget {
         },
       ),
     );
-  }
-}
-
-class MySearchDelegate extends SearchDelegate {
-  List<String> searchResults = [];
-
-  @override
-  Widget? buildLeading(BuildContext context) => IconButton(
-        onPressed: () => close(context, null), //close searchbar
-        icon: const Icon(Icons.arrow_back),
-      );
-
-  @override
-  List<Widget>? buildActions(BuildContext context) => [
-        IconButton(
-          onPressed: () {
-            if (query.isEmpty) {
-              close(context, null);
-            } //close searchbar
-            else {
-              query = '';
-            }
-          },
-          icon: const Icon(Icons.clear),
-        ),
-      ];
-
-  @override
-  Widget buildResults(BuildContext context) => Center(child: Text(query));
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    List<String> suggestions = searchResults.where((searchResult) {
-      final result = searchResult.toLowerCase();
-      final input = query.toLowerCase();
-
-      return result.contains(input);
-    }).toList();
-
-    return ListView.builder(
-        itemCount: suggestions.length,
-        itemBuilder: (context, index) {
-          final suggestion = suggestions[index];
-
-          return ListTile(
-              title: Text(suggestion),
-              onTap: () {
-                query = suggestion;
-
-                showResults(context);
-              });
-        });
   }
 }
